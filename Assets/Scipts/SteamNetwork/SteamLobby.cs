72ccb2deec1169ad5b5f84e3f3690e73aa5a33e1
Frustman <youhk40@naver.com>
2023-12-25 04:50:11 +0900

    Feat : Character Controller

--- Assets/Scipts/SteamNetwork/SteamLobby.cs
+++ Assets/Scipts/SteamNetwork/SteamLobby.cs
@@ -1,18 +1,75 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
+using Steamworks;
+using UnityEngine.UI;
 
 public class SteamLobby : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private NetworkManager networkManager;
+
+    public GameObject hostButton = null;
+
+    protected Callback<LobbyCreated_t> lobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequested;
+    protected Callback<LobbyEnter_t> lobbyEntered;
+
+    private const string HostAddressKey = "HostAddressKey";
+    
+    private void Start()
     {
-        
+        networkManager = GetComponent<NetworkManager>();
+
+        if (!SteamManager.Initialized) return;
+
+        lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+        gameLobbyJoinRequested = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequested);
+        lobbyEntered = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnLobbyCreated(LobbyCreated_t callback)
     {
+        if (callback.m_eResult != EResult.k_EResultOK)
+        {
+            hostButton.SetActive(true);
+            return;
+        }
         
+        networkManager.StartHost();
+
+        SteamMatchmaking.SetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HostAddressKey,
+            SteamUser.GetSteamID().ToString());
     }
+        
+    private void OnGameLobbyJoinRequested(GameLobbyJoinRequested_t callback)
+    {
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+    }
+
+
+    public void HostLobby()
+    {
+        hostButton.SetActive(false);
+
+        SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, networkManager.maxConnections);
+    }
+
+    private void OnLobbyEntered(LobbyEnter_t callback)
+    {
+        if (NetworkServer.active)
+        {
+            return;
+        }
+
+        string hostAddress = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), HostAddressKey);
+
+        networkManager.networkAddress = hostAddress;
+        networkManager.StartClient();
+        
+        hostButton.SetActive(false);
+    }
+
+        
 }
