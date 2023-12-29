76b1df54e1d187f015d0f6de72654795a2d57a44
Frustman <youhk40@naver.com>
2023-12-30 04:07:54 +0900

    Feat : Interactor

--- Assets/Scripts/Interaction/RayInteractor.cs
+++ Assets/Scripts/Interaction/RayInteractor.cs
@@ -1,15 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RayInteractor : MonoBehaviour
 {
-    [Header("Settings")] 
-    [SerializeField] private Camera camera;
-
-    // Update is called once per frame
-    void Update()
+    [Header("Ray Settings")] 
+    [SerializeField] private LayerMask targetLayer;
+    [SerializeField] private List<RayInteractable> interactables = new List<RayInteractable>();
+    
+    
+    
+    public void OnTriggerEnter(Collider other)
     {
-        
+        if ((targetLayer.value & (1 << other.gameObject.layer)) > 0)
+        {
+            RayInteractable element = other.GetComponent<RayInteractable>();
+            if (element != null)
+            {
+                if(!interactables.Contains(element)) interactables.Add(element);
+            }
+        }
+    }
+    
+    public void OnTriggerExit(Collider other)
+    {
+        if ((targetLayer.value & (1 << other.gameObject.layer)) > 0)
+        {
+            RayInteractable element = other.GetComponent<RayInteractable>();
+            if (element != null)
+            {
+                if(interactables.Contains(element)) interactables.Remove(element);
+            }
+        }
     }
 }
